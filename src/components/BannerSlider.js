1acdfed1130ad6b6b85bf9737d22b8c810d4b777
Chukwudi Nweze <chukwudinwez@gmail.com>
2021-04-17 11:08:40 +0100

    updated homepage

--- src/components/BannerSlider.js
+++ src/components/BannerSlider.js
@@ -9,7 +9,7 @@ const bannerDatas = [
     img: ImageSlide,
     heading: "Rent a home",
     detail:
-      "We're creating a seamless online experience for people in nsukka-from shopping on the largest network, to applyiing, to paying rent",
+      "We're creating a seamless online experience for people in nsukka, from shopping on the largest network, to applyiing and paying for rent",
     button: "rent home",
   },
   {
@@ -21,45 +21,38 @@ const bannerDatas = [
   },
   {
     img: ImageSlide,
-    heading: "sell your home/land",
-    detail: "we're creating a seamless online experience",
+    heading: "Rent a home",
+    detail:
+      "We're creating a seamless online experience for people in nsukka, from shopping on the largest network, to applyiing and paying for rent",
     button: "rent home",
   },
   {
     img: ImageSlide,
-    heading: "rent a home with just your phone at ease",
-    detail: "we're creating a seamless online experience",
-    button: "rent home",
-  },
-  {
-    img: ImageSlide,
-    heading: "rent a home with just your phone at ease",
-    detail: "we're creating a seamless online experience",
+    heading: "Rent a home",
+    detail:
+      "We're creating a seamless online experience for people in nsukka, from shopping on the largest network, to applyiing and paying for rent",
     button: "rent home",
   },
 ];
 const BannerSlider = () => {
   return (
-    <Splide
-      options={{
-        type: "loop",
-        gap: "1rem",
-        autoplay: "true",
-        pauseOnHover: "false",
-        arrows: "!false",
-        height: "300px",
-      }}
-      hasSliderWrapper
-    >
-      {React.Children.toArray(
-        bannerDatas.map((bannerData) => {
-          const { img, heading, detail, button } = bannerData;
-          return (
-            <SplideSlide>
-              <div
-                class="slide__wrapper"
-                style={{ backgroundImage: `url(${img})` }}
-              >
+    
+      <Splide
+        options={{
+          type: "loop",
+          gap: "1rem",
+          autoplay: "true",
+          pauseOnHover: "false",
+          arrows: "!false",
+          height: "300px",
+        }}
+        hasSliderWrapper
+      >
+        {React.Children.toArray(
+          bannerDatas.map((bannerData) => {
+            const { img, heading, detail, button } = bannerData;
+            return (
+              <SplideSlide>
                 <div className="slide__item">
                   <h1 className="h1">{heading}</h1>
                   <p>{detail}</p>
@@ -67,12 +60,12 @@ const BannerSlider = () => {
                     <Link to="/homes">{button}</Link>
                   </button>
                 </div>
-              </div>
-            </SplideSlide>
-          );
-        })
-      )}
-    </Splide>
+              </SplideSlide>
+            );
+          })
+        )}
+      </Splide>
+    
   );
 };
 
