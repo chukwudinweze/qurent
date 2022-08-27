c0dd1beeadc351efe818d53f4d8d43a623349e9b
Chukwudi Nweze <45886893+chukwudinweze@users.noreply.github.com>
2022-08-27 22:06:45 +0100

    styling property detail component

--- src/components/PropertyDetailDescription.js
+++ src/components/PropertyDetailDescription.js
@@ -1,5 +1,11 @@
 import React from "react";
+import NairaSymbol from "./NairaSymbol";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import PhoneInTalkIcon from "@material-ui/icons/PhoneInTalk";
+import MessageIcon from "@material-ui/icons/Message";
+
 import "../Styles/PropertyDetailDescription.css";
+import ContactAgent from "./ContactAgent";
 
 const PropertyDetailDescription = () => {
   return (
@@ -7,11 +13,39 @@ const PropertyDetailDescription = () => {
       <h4 className>
         3 bedroom flat for sale at UNN maing gate with a chip wonderful price
       </h4>
-      <h5># 13,000,000</h5>
-      <p>icon Oyo, Ibadan, Bodija</p>
-      <div>
-        <button>call agen</button>
-        <button>chat agent</button>
+      <h5>
+        <NairaSymbol /> 13,000,000
+      </h5>
+      <div className="roomdetail__location">
+        <LocationOnIcon style={{ fontSize: "1rem", transform: "scale(1.2)" }} />
+        location
+      </div>
+      <div className="roomdetail__contact__detail">
+        <ContactAgent
+          typeOfContact="Tel"
+          phoneNumber={`08060281867`}
+          label="Call Agent"
+          icon={
+            <PhoneInTalkIcon
+              style={{
+                fontSize: "1.188rem",
+                marginRight: "0.313rem",
+              }}
+            />
+          }
+        />
+        <ContactAgent
+          typeOfContact="sms"
+          phoneNumber={`08060281867`}
+          label="Chat Agent"
+          title={`title`}
+          icon={
+            <MessageIcon
+              style={{ fontSize: "1.25rem", marginRight: "0.313rem" }}
+            />
+          }
+        />
+        <ContactAgent />
       </div>
     </article>
   );
