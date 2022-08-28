d11fd65573b271187f7709aed5cef1639e844889
Chukwudi Nweze <45886893+chukwudinweze@users.noreply.github.com>
2022-08-28 20:51:51 +0100

    styling property detail page

--- src/components/PropertyDetailFacilities.js
+++ src/components/PropertyDetailFacilities.js
@@ -1,18 +1,35 @@
 import React from "react";
+import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
+
+import "../Styles/propertDetailFacilities.css";
 
 const PropertyDetailFacilities = () => {
   return (
-    <article>
+    <article className="property__detail__facilities">
+      <h6>Facilities</h6>
       <ul>
-        <li>made in lagos</li>
-        <li>made in lagos</li>
-        <li>made in lagos</li>
-        <li>made in lagos</li>
-        <li>made in lagos</li>
-        <li>made in lagos</li>
-        <li>made in lagos</li>
-        <li>made in lagos</li>
-        <li>made in lagos</li>
+        <li>
+          <CheckCircleOutlineIcon
+            style={{ /*fontSize: "14px",*/ color: "#20c063" }}
+          />
+          Balcony
+        </li>
+        <li>
+          <CheckCircleOutlineIcon style={{ fontSize: "14px" }} />
+          Car park
+        </li>
+        <li>
+          <CheckCircleOutlineIcon style={{ fontSize: "14px" }} />
+          Prepaid meter
+        </li>
+        <li>
+          <CheckCircleOutlineIcon style={{ fontSize: "14px" }} />
+          Post paid meter
+        </li>
+        <li>
+          <CheckCircleOutlineIcon style={{ fontSize: "14px" }} />
+          Security
+        </li>
       </ul>
     </article>
   );
---
+++ src/components/PropertyDetailedHighlight.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PropertyDetailedHighlight = () => {
+  return (
+    <article>pop celing Tiled floor pacling space balcony wardrope</article>
+  );
+};
+
+export default PropertyDetailedHighlight;
