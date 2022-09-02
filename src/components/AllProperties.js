e1e449bfad5debdf4083ca7ad9145d9f06d4e3e1
Chukwudi Nweze <45886893+chukwudinweze@users.noreply.github.com>
2022-09-02 17:07:31 +0100

    building search function

--- src/components/AllProperties.js
+++ src/components/AllProperties.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Loading from "./Loading";
 import { useSelector } from "react-redux";
 import SingleProperty from "./SingleProperty";
