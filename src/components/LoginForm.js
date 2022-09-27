34e3acfbd0e6f4edab6101b4add914236f49ac6d
Chukwudi Nweze <45886893+chukwudinweze@users.noreply.github.com>
2022-09-27 13:29:22 +0100

    modifying auth component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -12,7 +12,6 @@ import { useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 
 const LoginForm = () => {
-  console.log("gggggggggggggggggggggggggggggggggggg");
   const [login, setLogin] = useState(true);
 
   const dispatch = useDispatch();
@@ -21,7 +20,7 @@ const LoginForm = () => {
   const loading = useSelector((state) => state.uiInteraction.authLoading);
   const error = useSelector((state) => state.uiInteraction.errorAuth);
   const errorMsg = useSelector((state) => state.uiInteraction.errorMsg);
-  const isLoggedIn = useSelector((state) => state.user.token);
+  const isLoggedIn = useSelector((state) => state.user);
 
   // initial formik values
   const initialValues = {
@@ -36,93 +35,95 @@ const LoginForm = () => {
   const onSubmit = (value, { resetForm }) => {
     dispatch(setUser(value, url));
   };
-  console.log("token is", isLoggedIn);
+
+  if (!isLoggedIn) {
+    return (
+      <section className="register__form__section">
+        <article className="register__form__article">
+          <div className="logo__image">
+            <img src={Logo} alt="logo" />
+          </div>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+          >
+            {(formik) => {
+              return (
+                <Form>
+                  <strong>{login ? "Login" : "Welcome to qurent"}</strong>
+                  <p>
+                    {login
+                      ? "Kindly enter your details to login"
+                      : "Kindly enter correct details to sign up"}
+                  </p>
+                  <div className="login__email">
+                    <TextField
+                      error={formik.touched.email && formik.errors.email}
+                      id="email"
+                      label="Email Address"
+                      variant="outlined"
+                      value={formik.values.email || ""}
+                      onChange={formik.handleChange}
+                      helperText={formik.touched.email && formik.errors.email}
+                      fullWidth
+                      size="medium"
+                    />
+                  </div>
+                  <div className="login__password">
+                    <TextField
+                      error={formik.touched.password && formik.errors.password}
+                      id="password"
+                      label="Password"
+                      variant="outlined"
+                      value={formik.values.password || ""}
+                      onChange={formik.handleChange}
+                      helperText={
+                        formik.touched.password && formik.errors.password
+                      }
+                      fullWidth
+                      size="medium"
+                    />
+                  </div>
+                  <div className="login__button">
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      disableElevation
+                      fullWidth
+                      size="large"
+                      style={{ background: "#20c063", color: "#fff" }}
+                      disabled={loading}
+                    >
+                      {login && !loading && !error && "Login"}
+                      {!login && !loading && !error && "Sign up"}
+                      {!error && loading && "Authenticating"}
+                      {error && "Retry"}
+                    </Button>
+                  </div>
+                </Form>
+              );
+            }}
+          </Formik>
+          <p className="switch__state">
+            {login ? "Not registered yet?" : "Already registered?"}
+            <Button
+              style={{ color: "#20c063" }}
+              onClick={() => {
+                setLogin(!login);
+              }}
+            >
+              {login ? "Sign up" : "Login"}
+            </Button>
+          </p>
+          <p className="error">{error && errorMsg}</p>
+        </article>
+      </section>
+    );
+  }
   if (isLoggedIn) {
     return <Redirect to="/" />;
   }
-  return (
-    <section className="register__form__section">
-      <article className="register__form__article">
-        <div className="logo__image">
-          <img src={Logo} alt="logo" />
-        </div>
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={onSubmit}
-        >
-          {(formik) => {
-            return (
-              <Form>
-                <strong>{login ? "Login" : "Welcome to qurent"}</strong>
-                <p>
-                  {login
-                    ? "Kindly enter your details to login"
-                    : "Kindly enter correct details to sign up"}
-                </p>
-                <div className="login__email">
-                  <TextField
-                    error={formik.touched.email && formik.errors.email}
-                    id="email"
-                    label="Email Address"
-                    variant="outlined"
-                    value={formik.values.email || ""}
-                    onChange={formik.handleChange}
-                    helperText={formik.touched.email && formik.errors.email}
-                    fullWidth
-                    size="medium"
-                  />
-                </div>
-                <div className="login__password">
-                  <TextField
-                    error={formik.touched.password && formik.errors.password}
-                    id="password"
-                    label="Password"
-                    variant="outlined"
-                    value={formik.values.password || ""}
-                    onChange={formik.handleChange}
-                    helperText={
-                      formik.touched.password && formik.errors.password
-                    }
-                    fullWidth
-                    size="medium"
-                  />
-                </div>
-                <div className="login__button">
-                  <Button
-                    type="submit"
-                    variant="contained"
-                    disableElevation
-                    fullWidth
-                    size="large"
-                    style={{ background: "#20c063", color: "#fff" }}
-                    disabled={loading}
-                  >
-                    {login && !loading && !error && "Login"}
-                    {!login && !loading && !error && "Sign up"}
-                    {!error && loading && "Authenticating"}
-                    {error && "Retry"}
-                  </Button>
-                </div>
-              </Form>
-            );
-          }}
-        </Formik>
-        <p className="switch__state">
-          {login ? "Not registered yet?" : "Already registered?"}
-          <Button
-            style={{ color: "#20c063" }}
-            onClick={() => {
-              setLogin(!login);
-            }}
-          >
-            {login ? "Sign up" : "Login"}
-          </Button>
-        </p>
-        <p className="error">{error && errorMsg}</p>
-      </article>
-    </section>
-  );
 };
 
 export default LoginForm;
