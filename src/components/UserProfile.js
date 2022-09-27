ff34fd52aecd387ff137449750f4fcfd7618ce70
Chukwudi Nweze <45886893+chukwudinweze@users.noreply.github.com>
2022-09-27 21:41:18 +0100

    adding profile settings component

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,14 +1,33 @@
 import React from "react";
 import PageHeader from "./PageHeader";
 import "../Styles/Userprofile.css";
+import { useState } from "react";
 
 const UserProfile = () => {
+  const [settingsBtnUnderline, setSettingsBtnUnderline] = useState(false);
+  const [detailBtnUnderline, setDetailBtnUnderline] = useState(false);
   return (
     <section className="profile__details">
       <PageHeader titleLeft="Profile" />
       <nav>
-        <button>Settings</button>
-        <button>My Adverts</button>
+        <button
+          className={`${
+            settingsBtnUnderline
+              ? "underline profile__details_btn"
+              : "profile__details_btn"
+          }`}
+        >
+          My Adverts
+        </button>
+        <button
+          className={`${
+            detailBtnUnderline
+              ? "underline profile__details_btn "
+              : "profile__details_btn"
+          }`}
+        >
+          Settings
+        </button>
       </nav>
     </section>
   );
